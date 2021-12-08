f13312a995fe24d1424527c1320c3368d9d38bd7
lixingjia <623469848@qq.com>
2021-12-08 16:58:26 +0800

    [feat] - 增加fastjson依赖、openfeign依赖 - 增加feign配置

---
+++ src/main/java/com/github/wecomsdk/WeComSdkApplication.java
@@ -0,0 +1,15 @@
+package com.github.wecomsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class WeComSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeComSdkApplication.class, args);
+	}
+
+}
