8d6781473e8d057248d4781a03aba1e829329228
lixingjia <623469848@qq.com>
2021-12-09 13:59:32 +0800

    [feat] 配置forest

--- src/main/java/com/github/wecomsdk/WeComSdkApplication.java
+++ src/main/java/com/github/wecomsdk/WeComSdkApplication.java
@@ -1,14 +1,15 @@
 package com.github.wecomsdk;
 
+import com.dtflys.forest.springboot.annotation.ForestScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@ForestScan
 public class WeComSdkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeComSdkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeComSdkApplication.class, args);
+    }
 
 }
